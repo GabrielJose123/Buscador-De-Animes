4e5942db759c0989e4f6ac84a92aa4cfb65b0b07
GabrielJose123 <gabrieljoseroberto@hotmail.com>
2025-03-20 17:54:09 -0300

    20/03

--- js/js.js
+++ js/js.js
@@ -1,7 +1,14 @@
 
 const texto = document.getElementById("busca")
 
+const mostra = document.getElementById("mostra")
+
 let AnimesPesquisados = []
+
+let animeDiv;
+
+let animeMarcado = false;
+
 
 async function PegarUrl() {
     const src = document.getElementById("busca")
@@ -11,62 +18,141 @@ async function PegarUrl() {
    
     const response = await fetch(url)
     if (src.value !== "") { 
-       
-
-        if (response.status === 200) {
+       if (response.status === 200) {
             const data = await response.json()
 
             if (data.data.length === 0) {
-                alert("deu erro")
+                console.log("deu erro")
             } else if (!AnimesPesquisados.includes(src.value)) {
+                let imagem;
+                let titulo;
+                let ep;
+                let movie = false;
+                let year;
+                
 
-      
-               
-                const anime = data.data[0]
-        
-                const imagem = anime.images.jpg.image_url;
-        
-                console.log(imagem)
-        
+                const anime = await data.data[0]  
+                if (  anime.images?.jpg?.image_url !== null) {
+                     imagem =  anime.images.jpg.image_url; 
+                    console.log(imagem)
+                }else {
+                    console.log("imagens invalidadas na api")
+                }
+                if ( anime.title !== null) {
+                     titulo =  anime.title;
+                }else {
+                    console.log("titulo invalido")
+                }
+                if (  anime.episodes !== null) {
+                     ep =  anime.episodes;
+                     if (ep == 1) {
+                        movie = true;
+                        if (anime.duration != null) {
+                            ep = anime.duration;
+                        }
+                     }
+                }else {
+                    console.log("numero de episodios nao encontrado")
+                }
+                if (anime.year !== null) {
+                    year = anime.year
+                }else{
+                    if (anime.aired.prop.from.year != null || anime.aired.prop.from.year != undefined) {
+                        year = anime.aired.prop.from.year
+                    }else {
+                        year = ""
+                    }
+                }
+                
+                const textEp = document.createElement("p")
+                
+                const text = document.createElement("p")
+            
                 const imagemDoAnime = document.createElement("img")
                 imagemDoAnime.src = imagem
-                imagemDoAnime.style.width = "100%"
-                imagemDoAnime.style.height = "100%"
-                imagemDoAnime.style.objectFit = "cover"
-                 
-                const div = document.getElementById("anime")
-                div.appendChild(imagemDoAnime)
+                imagemDoAnime.style.width = "100%";
+                imagemDoAnime.style.height = "100%";
+                imagemDoAnime.style.objectFit = "cover";
                 
+                animeDiv = document.createElement("div")
+                animeDiv.id = `${src.value}`
+            
+                console.log(animeDiv.id)
+                const DivName = animeDiv.getAttribute(`${src}`)
+                animeDiv.appendChild(text)
+                mostra.appendChild(animeDiv)
+               
+                animeDiv.classList.add("class")
+               
+                animeDiv.appendChild(imagemDoAnime)
+                
+                text.innerHTML = titulo
+                
+                animeDiv.appendChild(textEp)
+            
                 AnimesPesquisados.push(src.value)
-
+            
                 console.log(AnimesPesquisados)
-
+            
                 src.value = ""
                 
-                
+                console.log(document.querySelectorAll(".class").length)
 
+               
+
+              
                 
-    
+            
+                if (year == null) {
+                    textEp.innerHTML = `Lançamento: Episódios:${ep}`
+                   
+                }
+                else if (ep == null && movie == false) {
+                    textEp.innerHTML = `Lançamento: ${year}<br>Episódios:`
+                }
+                else if (ep == null && movie == true) {
+                     textEp.innerHTML = `Lançamento: ${year}<br>Duração:`
+                }
+                else if (ep == null && year) {
+                    textEp.innerHTML = `Lançamento: ${year}<br>Episódios:`
+                }else if (ep !==null && movie == true) {
+                     textEp.innerHTML = `Lançamento: ${year}<br>Duração: ${ep}`
+                }
+                else {
+                    textEp.innerHTML = `Lançamento: ${year}<br>Episódios: ${ep}`
+                }
             }else {
                 alert("esse anime ja foi adicionado a lista     ")
             }
         }
     } else {
-        alert("coloque algo")
+        alert("digite algo")
     }
 }
 
-texto.addEventListener('focus',function(focus){
-    document.addEventListener('keydown', function(event) {
+    texto.addEventListener('keydown', function(event) {
         if (event.key == 'Enter' || event.key == 'Tab') {
             PegarUrl()
         }
     })
-    
-    
-})
+
+
+
 
 function focar() {
     texto.focus()
 }
 
+mostra.addEventListener('click',function(event){
+    console.log("voce clicou")
+    animeDiv.style.border = "1px solid red"
+    animeMarcado = true
+
+})
+
+function Delete() {
+    if (animeMarcado = true) {
+        animeDiv.remove()
+    }
+    
+}
